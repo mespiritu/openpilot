2458133844c9cd6d693f6f6d77f281a79b8d655b
Greg Esmond <gernby2002@yahoo.com>
2019-01-10 10:40:07 -0600

    Fixed typo

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -134,11 +134,11 @@ class CarController(object):
     # *** compute control surfaces ***
     BRAKE_MAX = 1024/4
     if CS.CP.carFingerprint in (CAR.ACCORD, CAR.ACCORD_15, CAR.ACCORDH, CAR.ACURA_ILX):
-      STEER_MAX = 0XF00
+      STEER_MAX = 0xF00
     elif CS.CP.carFingerprint in (CAR.CRV, CAR.ACURA_RDX):
       STEER_MAX = 0x3e8  # CR-V only uses 12-bits and requires a lower value (max value from energee)
     else:
-      STEER_MAX = 0X1000
+      STEER_MAX = 0x1000
 
     # steer torque is converted back to CAN reference (positive when steering right)
     apply_gas = clip(actuators.gas, 0., 1.)
