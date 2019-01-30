9d716926830ff4268c64464cf9ae51a35147091a
ErichMoraga <33645296+ErichMoraga@users.noreply.github.com>
2019-01-30 10:57:20 -0600

    Fan speed modification for Noctua modded EONs.
    (cherry picked from commit 4b61187469f52c1dbd97e35b24bca29333e6ca55)
    (cherry picked from commit 267a88f214e31696a8ebfcbbd56fe32cc4587381)
    (cherry picked from commit 3a3d13f482e2c733120245809c235f4b3e29a2b0)

--- selfdrive/thermald.py
+++ selfdrive/thermald.py
@@ -80,7 +80,7 @@ _TEMP_THRS_H = [50., 65., 80., 10000]
 # temp thresholds to control fan speed - low hysteresis
 _TEMP_THRS_L = [42.5, 57.5, 72.5, 10000]
 # fan speed options
-_FAN_SPEEDS = [0, 16384, 32768, 65535]
+_FAN_SPEEDS = [0, 65535, 65535, 65535] # Noctua fan is super quiet, so it can run on high most of the time.
 # max fan speed only allowed if battery is hot
 _BAT_TEMP_THERSHOLD = 45.
 
